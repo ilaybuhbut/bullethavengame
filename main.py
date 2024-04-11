b02bfe67143763504a1df249ed1189052179c690
ilaybuhbut <ilayforshcool@gmail.com>
2024-04-11 13:53:17 +0300

    added enemies that move better and you can die maybe

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ width = 50
 height = 50
 crab_x = randrange(window_width)
 crab_y = randrange(window_height)
-crab_speed = 2
+crab_speed = 1
 vel = 3
 fps=0
 last_time = time.time()
@@ -41,6 +41,8 @@ def moveCrab():
     for crab in crab:
         crabs[1]+=crab_speed
 
+last_time = pygame.time.get_ticks()
+
 while running:
 
 
@@ -57,16 +59,24 @@ while running:
     if keys[pygame.K_RIGHT] and x < 500-width:
         x += vel
 
-    crab_x += crab_speed
-    crab_y += crab_speed
-
-    if crab_x >= width:
+    delta_time = pygame.time.get_ticks() - last_time
+    last_time = pygame.time.get_ticks()
+    
+    if crab_x < window_width / 2:
+        crab_x += crab_speed
+    elif crab_x > window_width / 2:
+        crab_x -= crab_speed
+    if crab_y< window_height / 2:
+        crab_y+= crab_speed
+    elif crab_y> window_height / 2:
+        crab_y-= crab_speed
+    if (crab_x , crab_y) == (window_width / 2, window_height / 2):
         crab_x = randrange(window_width)
         crab_y = randrange(window_height)
+
     game_display.blit(bg_image, (0, 0))
     game_display.blit(player, (x,y))
     generateEnemies(crab_x , crab_y)
-    crab
 
 
     pygame.display.update()
@@ -76,9 +86,5 @@ while running:
         fps=0
         last_time = time.time()
 
-    fps+=1
-    txtsurf = font.render("FPS:"+str(real_fps), True, black)
-    screen.blit(txtsurf,(380,10))
-    pygame.display.flip()
 
 pygame.quit()
